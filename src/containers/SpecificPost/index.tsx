073b8aa61ddb68de27349dc825ee39c97dfbe5b6
Valdeadmario <vladimir.vstup@gmail.com>
2019-09-12 21:38:49 +0300

    Clean code

--- src/containers/SpecificPost/index.tsx
+++ src/containers/SpecificPost/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { FaTimes } from "react-icons/fa";
