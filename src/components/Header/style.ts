e2df9023439ebecf5c5dc9589b4913130d088aa3
Valdeadmario <vladimir.vstup@gmail.com>
2019-09-12 16:19:56 +0300

    Create page header

---
+++ src/components/Header/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  padding: 20px;
+  background: #222222;
+  min-height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Name = styled.span`
+  font-size: 2rem;
+`;
